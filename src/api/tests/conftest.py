211a8903e7c969810f1276ada4366557c0cfbf1f
lyncir <lyncir@gmail.com>
2017-06-26 17:58:09 +0800

    update conftest

--- src/api/tests/conftest.py
+++ src/api/tests/conftest.py
@@ -5,5 +5,10 @@ import asyncio
 
 @pytest.fixture
 def loop():
-    loop = asyncio.get_event_loop()
-    return loop
+    # set up
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    yield loop
+
+    # clean up
+    loop.close()
